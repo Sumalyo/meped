ccb78b9b11f39f194a16ca3f195edd393490b903
Sumalyo Datta <39642083+Sumalyo@users.noreply.github.com>
2020-11-23 17:14:19 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,10 @@
+import requests as req
+import subprocess as sp
+def download_url(url):
+  res = req.get(url)
+  #print(res)
+  #print(res.content)
+  return res
 with open ("output.out","wb") as out_file:
   tar = download_url('https://raw.githubusercontent.com/Sumalyo/meped/master/a.out').content
   out_file.write(tar)
